5c067e69d8fe02271dc11859b2a41159720d4e8f
mohammad <mohammadabedi1179@gmail.com>
2022-05-29 12:01:07 +0430

    add getScores and it's completed

--- Inference_Class.py
+++ Inference_Class.py
@@ -1,4 +1,4 @@
-#%conda activate tf-gpu
+#python3
 import sys
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Conv2D, ReLU, BatchNormalization, Dropout, DepthwiseConv2D, Add, Flatten
@@ -7,19 +7,14 @@ from tensorflow.keras.initializers import glorot_uniform
 from tensorflow.keras import Input, Model
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 import matplotlib.pyplot as plt
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.layers.experimental.preprocessing import RandomZoom, CenterCrop, RandomContrast
-from tensorflow.keras.metrics import AUC
+from tensorflow.keras.layers.experimental.preprocessing import RandomContrast
 from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.callbacks import ModelCheckpoint
 import numpy as np
 import sklearn.metrics
-import tensorflow.keras.backend as kbe
-from tensorflow.keras import backend as K
-from PIL import Image
 import glob
 import cv2
-
+import pandas as pd
 class Inference():
 
     def __init__(self, path : str, train= False, checkpoint_path= 'tmp/hichi', load_model=False):
@@ -32,19 +27,20 @@ class Inference():
             Inference.load_model()
     
     def dataset(self):
-        BATCH_SIZE = 128
+        self.calass_names = ['alphabet', 'number']
+        self.batch_size = 128
         IMG_SIZE = (32, 32)
         training_directory = f"{self.path}/train"
         valid_directory = f"{self.path}/valid"
 
         self.train_dataset = image_dataset_from_directory(training_directory,
                                                     shuffle=True,
-                                                    batch_size=BATCH_SIZE,
+                                                    batch_size=self.batch_size,
                                                     image_size=IMG_SIZE,
                                                     seed=42)
         self.validation_dataset = image_dataset_from_directory(valid_directory,
                                                     shuffle=True,
-                                                    batch_size=BATCH_SIZE,
+                                                    batch_size=self.batch_size,
                                                     image_size=IMG_SIZE,
                                                     seed=42)
     
@@ -170,12 +166,6 @@ class Inference():
 
         model = tf.keras.models.load_model(model_path)
         self.model = model
-        
-        """elif model_weights_path != None:
-            model = Inference.conv_model()
-            model.compile(optimizer=Adam(lr=base_learning_rate), loss=BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
-            model.load_weights(model_weights_path)
-            self.model = model"""
 
         return model
     
@@ -185,11 +175,6 @@ class Inference():
         images = Inference.preprocess_image(images_path)
         predictions = model.predict(images)
         predictions = np.round(predictions, decimals=3)
-
-        """elif model_weights_path != None:
-            load_model(self, model_weights_path=model_weights_path)
-        images = Inference.preprocess_image(images_path)
-        predictions = self.model.predict(images)"""
 
         return predictions
 
@@ -207,3 +192,109 @@ class Inference():
 
         return images
     
+    def eer(numpy_labels, numpy_labels_pred, positive_label=1):
+
+        fpr, tpr, threshold = sklearn.metrics.roc_curve(numpy_labels, numpy_labels_pred, pos_label=positive_label)
+        fnr = 1 - tpr
+
+        eer_threshold = threshold[np.nanargmin(np.absolute((fnr - fpr)))]
+
+        eer_1 = fpr[np.nanargmin(np.absolute((fnr - fpr)))]
+        eer_2 = fnr[np.nanargmin(np.absolute((fnr - fpr)))]
+
+        eer = eer_1/2 + eer_2/2
+
+        return eer
+    
+    def compute_eer(self, model_path='tmp/models/checkpoint_alpha=0.01_bn_aug(rc)_do(1)'):
+        Inference.dataset(self)
+        Inference.load_model(self, model_path=model_path)
+
+        train_numpy_labels, train_numpy_labels_pred = Inference.create_label_and_pred(self, self.train_dataset, 19980)
+        valid_numpy_labels, valid_numpy_labels_pred = Inference.create_label_and_pred(self, self.validation_dataset, 830)
+
+        train_eer = Inference.eer(train_numpy_labels, train_numpy_labels_pred)
+        valid_eer = Inference.eer(valid_numpy_labels, valid_numpy_labels_pred)
+        
+        print(f'EER value for training dataset is : {train_eer}')
+        print(f'EER value for validation dataset is : {valid_eer}')
+
+
+    def create_label_and_pred(self, dataset, dataset_size):
+
+        numpy_images = np.zeros([dataset_size, 32, 32, 3])
+        numpy_labels = np.zeros([dataset_size, 1])
+        k = 0
+
+        for images, labels in dataset.take(np.floor(dataset_size/self.batch_size) + 1):
+            images_n = images.numpy()
+            labels_n = labels.numpy()
+            for i in range(labels_n.shape[0]):
+                numpy_labels[i + k*self.batch_size] = labels_n[i]
+                numpy_images[i + k*self.batch_size, :, :, :] = images_n[i]
+            k = k + 1
+        numpy_labels_pred = self.model.predict(numpy_images)
+
+        return numpy_labels, numpy_labels_pred
+    def scores(self, model_path='tmp/models/checkpoint_alpha=0.01_bn_aug(rc)_do(1)'):
+        Inference.dataset(self)
+        Inference.load_model(self,model_path=model_path)
+
+        train_numpy_labels, train_numpy_labels_pred = Inference.create_label_and_pred(self, self.train_dataset, 19980)
+        valid_numpy_labels, valid_numpy_labels_pred = Inference.create_label_and_pred(self, self.validation_dataset, 830)
+
+        train_numpy_labels_pred = np.round(train_numpy_labels_pred, decimals=0)
+        valid_numpy_labels_pred = np.round(valid_numpy_labels_pred, decimals=0)
+
+        scores={}
+
+        train_difference = train_numpy_labels_pred - train_numpy_labels
+        valid_difference = valid_numpy_labels_pred - valid_numpy_labels
+        training_overal_accuracy = 1 - np.sum(np.abs(train_difference))/train_numpy_labels.shape[0]
+        valid_overal_accuracy = 1 - np.sum(np.abs(valid_difference))/valid_numpy_labels.shape[0]
+
+        train_cache = np.array([0, 0, 0, 0])
+
+        for i in range(train_difference.shape[0]):
+            if train_difference[i] == 1:
+                train_cache[0] = train_cache[0] + 1
+            elif train_difference[i] == -1:
+                train_cache[1] = train_cache[1] + 1
+            else:
+                if train_numpy_labels_pred[i] == 0:
+                    train_cache[2] = train_cache[2] + 1
+                else:
+                    train_cache[3] = train_cache[3] + 1
+        
+        valid_cache = np.array([0, 0, 0, 0])
+
+        for i in range(valid_difference.shape[0]):
+            if valid_difference[i] == 1:
+                valid_cache[0] = valid_cache[0] + 1
+            elif valid_difference[i] == -1:
+                valid_cache[1] = valid_cache[1] + 1
+            else:
+                if valid_numpy_labels_pred[i] == 0:
+                    valid_cache[2] = valid_cache[2] + 1
+                else:
+                    valid_cache[3] = valid_cache[3] + 1
+
+        scores['training overal accuracy'] = training_overal_accuracy
+        scores['validation overal accuracy'] = valid_overal_accuracy
+        scores['training alphabet accuracy'] = train_cache[2]/(train_cache[2] + train_cache[0])
+        scores['training numeric accuracy'] = train_cache[3]/(train_cache[1] + train_cache[3])
+        scores['validation alphabet accuracy'] = valid_cache[2]/(valid_cache[2] + valid_cache[0])
+        scores['validation numeric accuracy'] = valid_cache[3]/(valid_cache[1] + valid_cache[3])
+
+        score_df = pd.DataFrame(scores)
+
+        return score_df
+
+    def getScores(self, images_path, model_path='tmp/models/checkpoint_alpha=0.01_bn_aug(rc)_do(1)'):
+        preds = Inference.predict(self, images_path=images_path, model_path=model_path)
+        numeric_preds = preds
+        alphabetic_preds = 1 - preds
+        scores = {'Alphabetic probability' : list(alphabetic_preds), 'Numeric probability' : list(numeric_preds)}
+        scores_df = pd.DataFrame(scores)
+        scores_df.to_html("scores.html")
+        return scores_df
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from Inference_Class import Inference
 #print(Inference("E:/Educate/Work/model").predict('../Datasets/work/fashion/valid/numeric/',model_weights_path='E:/Educate/Work/model/tmp/checkpoint_alpha=0.1_bn_aug(rc)_do(1)'))
-print(Inference("model/").predict("model/valid/numeric", model_path='tmp/models/checkpoint_alpha=0.01_bn_aug(rc)_do(1)'))
+Inference("model/").getScores(images_path='model/valid/alphabet',model_path='tmp/models/checkpoint_alpha=0.01_bn_aug(rc)_do(1)')
